a815a306a2499a06c43d16367ae52fee9a7b5c10
Thomas Braccia <bracciata@gmail.com>
2021-03-06 22:15:36 -0600

    Added Tom Bot that is working to this repo but work for him has been in other repo and began work on site and added first email template.

--- iep-assistant-react/src/App.js
+++ iep-assistant-react/src/App.js
@@ -1,23 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import ReactDOM from 'react-dom';
 
 function App() {
+  const iFrameStlye = {
+    "min-width": "400px", 
+    "width": "100%", 
+    "min-height": "500px",
+    "height":"100%"
+  };
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1> Tom the IEP Assistant</h1>
       </header>
+      <h2>Tom is hear to walk you through the progres of getting an IEP set up for yourself or somebody else in addition to reaching out if it is not being followed.
+      </h2>
+      <iframe src='https://webchat.botframework.com/embed/A11yIEPBotTomHack?s=3f_Gop7HYa8.sV2TEexNzndI-uwORGfcELx3V5a8ixz_gTvTnEXk9fI'  style={iFrameStlye}></iframe>
     </div>
   );
 }
